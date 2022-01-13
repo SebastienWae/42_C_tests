ff778ac2fc0c14058592fc02288a96e849c570dd
Sebastien Waegeneiere <swaegene@k0r3p6.42mulhouse.fr>
2022-01-13 17:24:43 +0100

    cleanup

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-find "$1" -type f -name "*.c" -exec norminette -R CheckForbiddenSourceHeader {} \;
-
-echo '\n'
-
-gcc -Wall -Werror -Wextra $(find $1 -type f -name "*.c" | tr '\n' ' ') "$2" && ./a.out
