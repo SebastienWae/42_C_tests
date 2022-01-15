acffbc2d33c88f694f37df0d84efb9811a6536b2
Sebastien Waegeneiere <swaegene@k0r2p6.42mulhouse.fr>
2022-01-15 12:55:47 +0100

    fix test for c01/ex08

--- c01/ex08.c
+++ c01/ex08.c
@@ -1,11 +1,17 @@
+#include <stdio.h>
+
 void ft_sort_int_tab(int *tab, int size);
 
 int	main(void)
 {
-	char	*tab = { 732, 3, 24, 1 };
+	int	tab[4] = { 234, 3, 24, 1 };
 
 	ft_sort_int_tab(tab, 4);
-	if (tab[0] == 1 && tab[1] == 3 && tab[2] == 24 && tab[3] == 732)
+	printf("%d\n", tab[0]);
+	printf("%d\n", tab[1]);
+	printf("%d\n", tab[2]);
+	printf("%d\n", tab[3]);
+	if (tab[0] == 1 && tab[1] == 3 && tab[2] == 24 && tab[3] == 234)
 	   return 0;
 	else
 		return 1;
