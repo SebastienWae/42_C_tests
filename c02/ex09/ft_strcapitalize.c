431187a9744a2444149b30f15468756fb537db0a
Sebastien Waegeneiere <swaegene@k0r2p6.42mulhouse.fr>
2022-01-18 19:04:15 +0100

    fix c02/ex09

--- c02/ex09/ft_strcapitalize.c
+++ c02/ex09/ft_strcapitalize.c
@@ -7,16 +7,20 @@ int	main(void)
 {
 	char	test1[] = "hello";
 	char	test2[] = "salut, Comment tu vas ? 42mots quarante-deux; cinquante+et+un";
+	char	test3[] = "salut, cOMMENT tu vas ? 42mots quaraNte-deux; ci..nquante+et+un";
 
 	ft_strcapitalize(test1);
 	if(strcmp(test1, "Hello"))
 		return (1);
 
 	ft_strcapitalize(test2);
-	printf("%s\n", test2);
 	if(strcmp(test2, "Salut, Comment Tu Vas ? 42mots Quarante-Deux; Cinquante+Et+Un"))
 		return (1);
 
+	ft_strcapitalize(test3);
+	printf("%s\n", test3);
+	if(strcmp(test3, "Salut, Comment Tu Vas ? 42mots Quarante-Deux; Ci..Nquante+Et+Un"))
+		return (1);
 
 	return (0);
 }
