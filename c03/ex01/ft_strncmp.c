70309e76fe1281ce0c54571aaf8d0592f6132022
Sebastien Waegeneiere <swaegene@k0r2p6.42mulhouse.fr>
2022-01-18 14:20:20 +0100

    add c03/ex01

---
+++ c03/ex01/ft_strncmp.c
@@ -0,0 +1,18 @@
+int ft_strncmp(char *s1, char *s2, unsigned int n);
+
+int	main(void)
+{
+	if (ft_strncmp("42", "44", 1) != 0)
+		return (1);
+
+	if (ft_strncmp("42\200", "42\0", 3) != 1)
+		return (1);
+
+	if (ft_strncmp("abc", "ab", 2) != 0)
+		return (1);
+
+	if (ft_strncmp("aaaaaa", "z", 1) != -1)
+		return (1);
+
+	return (0);
+}
