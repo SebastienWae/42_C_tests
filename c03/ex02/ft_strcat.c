f6440ffb78543501a5fe638e06340ac861aa2385
Sebastien Waegeneiere <swaegene@k0r2p6.42mulhouse.fr>
2022-01-18 14:42:39 +0100

    add c03/ex02

--- c03/ex02/ft_strcat.c
+++ c03/ex02/ft_strcat.c
@@ -1,6 +1,17 @@
+#include <string.h>
+#include <stdio.h>
+
 char *ft_strcat(char *dest, char *src);
 
-void	main(void)
+int	main(void)
 {
-	
+	char	dest[20] = "hello";
+	char	dest_test[20] = "hello";
+
+	ft_strcat(dest, "world");
+	strcat(dest_test, "world");
+	if (strcmp(dest, "helloworld") != 0 && strcmp(dest, dest_test) != 0)
+		return (1);
+
+	return (0);
 }
