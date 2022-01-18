c18e6984f8a52e57d1e21ea19cc022f9c9917cbb
Sebastien Waegeneiere <swaegene@k0r2p6.42mulhouse.fr>
2022-01-18 14:42:26 +0100

    improve tests for c03/ex01

--- c03/ex01/ft_strncmp.c
+++ c03/ex01/ft_strncmp.c
@@ -1,17 +1,27 @@
+#include <string.h>
+
 int ft_strncmp(char *s1, char *s2, unsigned int n);
 
 int	main(void)
 {
 	if (ft_strncmp("42", "44", 1) != 0)
 		return (1);
+	if (strncmp("42", "44", 1) != 0)
+		return (1);
 
 	if (ft_strncmp("42\200", "42\0", 3) != 1)
+		return (1);
+	if (strncmp("42\200", "42\0", 3) != 1)
 		return (1);
 
 	if (ft_strncmp("abc", "ab", 2) != 0)
 		return (1);
+	if (strncmp("abc", "ab", 2) != 0)
+		return (1);
 
 	if (ft_strncmp("aaaaaa", "z", 1) != -1)
+		return (1);
+	if (strncmp("aaaaaa", "z", 1) != -1)
 		return (1);
 
 	return (0);
