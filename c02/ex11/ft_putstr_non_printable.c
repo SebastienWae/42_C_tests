32b7f143277a8056e79d834e8660743078985f79
Sebastien Waegeneiere <swaegene@k0r3p6.42mulhouse.fr>
2022-01-19 16:34:45 +0100

    fix ex11

--- c02/ex11/ft_putstr_non_printable.c
+++ c02/ex11/ft_putstr_non_printable.c
@@ -2,5 +2,5 @@ void ft_putstr_non_printable(char *str);
 
 int	main(void)
 {
-	ft_putstr_non_printable("Coucou\ntu vas bien ?");
+	ft_putstr_non_printable("Coucou\ntu \x0f  vas bien \x9f \x81?");
 }
