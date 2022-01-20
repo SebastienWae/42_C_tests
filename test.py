e74cce98adfa7ec5d98c1f30143a5785b2f50862
Sebastien Waegeneiere <swaegene@k0r1p9.42mulhouse.fr>
2022-01-20 12:07:32 +0100

    fixe exercies testing order

--- test.py
+++ test.py
@@ -80,7 +80,7 @@ def run_test(project_path, project, exercise, watch, force):
         test_exercise(exercise, project_path, test_path, force)
         print("\n")
     else:
-        test_files = [f for f in glob.glob(f"{test_path}/ex*")]
+        test_files = sorted([f for f in glob.glob(f"{test_path}/ex*")])
         for ex in test_files:
             if(not test_exercise(os.path.basename(ex), project_path, test_path, force) and not force):
                 break;
