c028cc641d97da94d28307d0f0ed1fecdd1bc1d3
Sebastien Waegeneiere <swaegene@k0r1p9.42mulhouse.fr>
2022-01-20 14:27:32 +0100

    fixes for c03/ex03

--- c03/ex03/ft_strncat.c
+++ c03/ex03/ft_strncat.c
@@ -3,15 +3,31 @@
 
 char *ft_strncat(char *dest, char *src, unsigned int nb);
 
+/*
+ * ft_strncat
+ * append `s2` to `s1` up to `n`
+ * add terminating NUL
+ * return pointer to `s1`
+ */
+
 int	main(void)
 {
 	char	dest[20] = "hello";
-	char	dest_test[20] = "hello";
+	char	dest2[20] = "hello";
 
 	ft_strncat(dest, "world", 2);
-	strncat(dest_test, "world", 2);
-	if (strcmp(dest, "hellowo") != 0 && strcmp(dest, dest_test) != 0)
+	if (strcmp(dest, "hellowo") != 0)
+	{
+		printf("failed: does not append src to dst or append more than n char");
 		return (1);
+	}
+
+	ft_strncat(dest2, "aaaa", 1);
+	if (dest2[6] != '\0')
+	{
+		printf("failed: does not add terminating NUL");
+		return (1);
+	}
 
 	return (0);
 }
