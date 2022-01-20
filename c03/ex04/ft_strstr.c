f9791315d61859a16d37b0cf56510c1db8fcce8b
Sebastien Waegeneiere <swaegene@k0r1p9.42mulhouse.fr>
2022-01-20 14:36:59 +0100

    fixes for c03/ex04

--- c03/ex04/ft_strstr.c
+++ c03/ex04/ft_strstr.c
@@ -3,25 +3,35 @@
 
 char *ft_strstr(char *str, char *to_find);
 
+/*
+ * ft_strstr
+ * locate the first occurrence of `to_find` in `str`
+ * if `to_find` is empty return `str`
+ * if `to_find` is not in `str` return 0
+ * else return pointer to first char of the first occurrence
+ */
+
 int	main(void)
 {
-	char	*r;
-	char	*r_test;
+	char	test[] = "hello world";
 
-	r = ft_strstr("hello world", "wo");
-	r_test = strstr("hello world", "wo");
-	if (strcmp(r, r_test) != 0)
+	if(ft_strstr(test, "wo") != &test[6])
+	{
+		printf("failed: does not return pointer to first occurrence\n");
 		return (1);
+	}
 
-	r = ft_strstr("hello world", "");
-	r_test = strstr("hello world", "");
-	if (strcmp(r, r_test) != 0)
+	if(ft_strstr(test, "") != test)
+	{
+		printf("failed: does not return str if to_find is empty\n");
 		return (1);
+	}
 
-	r = ft_strstr("hello world", "zz");
-	r_test = strstr("hello world", "zz");
-	if (r != r_test)
+	if(ft_strstr(test, "zz") != 0)
+	{
+		printf("failed: does not return 0 when to_find is not in str");
 		return (1);
+	}
 
 	return (0);
 }
