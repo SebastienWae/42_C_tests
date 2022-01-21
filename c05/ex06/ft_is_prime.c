2cd9649f37514dd5690cd8ef9843e1e67348a0f3
Sebastien Waegeneiere <swaegene@k0r1p9.42mulhouse.fr>
2022-01-21 12:41:10 +0100

    c05/ex06 && c05/ex07

---
+++ c05/ex06/ft_is_prime.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int ft_is_prime(int nb);
+
+int	main(void)
+{
+	if(ft_is_prime(2) != 1)
+		return (1);
+
+	if(ft_is_prime(11) != 1)
+		return (1);
+		
+	if(ft_is_prime(97) != 1)
+		return (1);
+
+	if(ft_is_prime(200011327) != 1)
+		return (1);
+		
+	if(ft_is_prime(4) != 0)
+		return (1);
+
+	if(ft_is_prime(12) != 0)
+		return (1);
+
+	if(ft_is_prime(99) != 0)
+		return (1);
+}
---
+++ c05/ex07/ft_find_next_prime.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int ft_find_next_prime(int nb);
+
+int	main(void)
+{
+	if(ft_find_next_prime(2) != 2)
+		return (1);
+
+	if(ft_find_next_prime(11) != 11)
+		return (1);
+		
+	if(ft_find_next_prime(4) != 5)
+		return (1);
+
+	if(ft_find_next_prime(10) != 11)
+		return (1);
+
+}
