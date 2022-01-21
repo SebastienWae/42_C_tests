dcfd172dd446ebddf6c712e4f6eb044f648626a3
Sebastien Waegeneiere <swaegene@k0r1p9.42mulhouse.fr>
2022-01-21 10:05:22 +0100

    c04/ex04

--- c04/ex04/ft_putnbr_base.c
+++ c04/ex04/ft_putnbr_base.c
@@ -2,5 +2,11 @@ void ft_putnbr_base(int nbr, char *base);
 
 int	main(void)
 {
-	ft_putnbr_base(123, "0123456789abcdef");
+	ft_putnbr_base(123, "0123456789ABCDEF"); // 7B
+	ft_putnbr_base(123, "01"); // 1111011
+	ft_putnbr_base(-6546546, "01"); // -11000111110010001110010
+
+	ft_putnbr_base(123, "1");
+	ft_putnbr_base(123, "abcda");
+	ft_putnbr_base(123, "0+1");
 }
