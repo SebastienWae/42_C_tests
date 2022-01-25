90073ec17a6ffc1bad1cc7dec5a3d62110dce75f
Sebastien Waegeneiere <swaegene@k0r2p7.42mulhouse.fr>
2022-01-25 15:35:07 +0100

    fix c07/ex02

--- c07/ex02/ft_ultimate_range.c
+++ c07/ex02/ft_ultimate_range.c
@@ -9,35 +9,36 @@ int	main(void)
 	int **range;
 
 	range = malloc(sizeof(int*));
-	if(ft_ultimate_range(range, 0, 10) != 9 || (*range)[0] != 1 || (*range)[8] != 9)
+	if(ft_ultimate_range(range, 0, 10) != 10 && (*range)[0] != 0 && (*range)[9] != 9)
 		return (1);
 	free(range);
 
 	range = malloc(sizeof(int*));
-	if(ft_ultimate_range(range, -20, -10) != 9 || (*range)[0] != -19 || (*range)[8] != -11)
+	if(ft_ultimate_range(range, -20, -10) != 10 && (*range)[0] != -20 && (*range)[9] != -11)
 		return (1);
 	free(range);
 
 	range = malloc(sizeof(int*));
-	if(ft_ultimate_range(range, 10, 0) != -1)
+	if(ft_ultimate_range(range, 10, 0) != 0)
 		return (1);
 	free(range);
 
 	range = malloc(sizeof(int*));
-	if(ft_ultimate_range(range, 0, 0) != -1)
+	if(ft_ultimate_range(range, 0, 0) != 0)
 		return (1);
 	free(range);
 
 	range = malloc(sizeof(int*));
-	if(ft_ultimate_range(range, 0, 1) != -1)
+	if(ft_ultimate_range(range, 0, 1) != 1 && (*range)[0] != 0)
 		return (1);
 	free(range);
 
 	range = malloc(sizeof(int*));
-	if(ft_ultimate_range(range, 0, 2) != 1)
+	if(ft_ultimate_range(range, 0, 2) != 2)
 		return (1);
 	free(range);
 
-	//if(ft_ultimate_range(INT_MIN, INT_MAX)[0] != INT_MIN + 1)
-	//	return (1);
+	range = malloc(sizeof(int*));
+	if(ft_ultimate_range(range, INT_MIN, INT_MAX) != -1)
+		return (1);
 }
